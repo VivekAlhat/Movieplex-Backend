63885bf8ceaabcc2e9eab1e90f2aed2b0b2317dc
VivekAlhat <vivekalhat14@outlook.com>
2021-06-24 19:12:14 +0530

    Changed Base URI

--- routes/movies.js
+++ routes/movies.js
@@ -2,10 +2,12 @@ const express = require("express");
 const axios = require("axios").default;
 const moviesRouter = express.Router();
 
+const BASE_URI = "https://api.themoviedb.org/3";
+
 moviesRouter.get("/genres", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.API_KEY}&language=en-US`
+      `${BASE_URI}/genre/movie/list?api_key=${process.env.API_KEY}&language=en-US`
     )
     .then((response) => {
       res.json(response.data);
@@ -17,7 +19,7 @@ moviesRouter.get("/genres/:genreId", (req, res) => {
   const genreId = req.params.genreId;
   axios
     .get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`
+      `${BASE_URI}/discover/movie?api_key=${process.env.API_KEY}&with_genres=${genreId}`
     )
     .then((response) => {
       res.json(response.data);
@@ -25,10 +27,10 @@ moviesRouter.get("/genres/:genreId", (req, res) => {
     .catch((err) => res.json(err));
 });
 
-https: moviesRouter.get("/latest", (req, res) => {
+moviesRouter.get("/latest", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/latest?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/latest?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -39,7 +41,7 @@ https: moviesRouter.get("/latest", (req, res) => {
 moviesRouter.get("/nowplaying", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/now_playing?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -50,7 +52,7 @@ moviesRouter.get("/nowplaying", (req, res) => {
 moviesRouter.get("/popular", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -61,7 +63,7 @@ moviesRouter.get("/popular", (req, res) => {
 moviesRouter.get("/toprated", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -72,7 +74,7 @@ moviesRouter.get("/toprated", (req, res) => {
 moviesRouter.get("/upcoming", (req, res) => {
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -85,7 +87,7 @@ moviesRouter.get("/find/:movieId", (req, res) => {
 
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.API_KEY}&language=en-US`
+      `${BASE_URI}/movie/${movie_id}?api_key=${process.env.API_KEY}&language=en-US`
     )
     .then((response) => {
       res.json(response.data);
@@ -98,7 +100,7 @@ moviesRouter.get("/find/similar/:movieId", (req, res) => {
 
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/${movie_id}/similar?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/${movie_id}/similar?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -111,7 +113,7 @@ moviesRouter.get("/find/cast/:movieId", (req, res) => {
 
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/${movie_id}/credits?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -124,7 +126,7 @@ moviesRouter.get("/find/reviews/:movieId", (req, res) => {
 
   axios
     .get(
-      `https://api.themoviedb.org/3/movie/${movie_id}/reviews?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      `${BASE_URI}/movie/${movie_id}/reviews?api_key=${process.env.API_KEY}&language=en-US&page=1`
     )
     .then((response) => {
       res.json(response.data);
@@ -136,7 +138,7 @@ moviesRouter.post("/search/", (req, res) => {
   const query = req.body.query;
   axios
     .get(
-      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
+      `${BASE_URI}/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
     )
     .then((response) => {
       res.json(response.data);
